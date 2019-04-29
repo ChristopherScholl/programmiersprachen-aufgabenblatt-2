ca4697e4f01dc72f9d62fc355159f07200d82c4c
ChristopherScholl <christopher.scholl@online.de>
2019-04-29 15:52:22 +0200

    2.4 functional

--- source/tests.cpp
+++ source/tests.cpp
@@ -28,6 +28,27 @@ TEST_CASE("describe_Vec2", "[Vec2]") {
   c /= 5;
   REQUIRE(c.x_ == Approx(1.0f));
   REQUIRE(c.y_ == Approx(1.0f));
+
+  //2.3
+  Vec2 d = b + c;
+  REQUIRE(d.x_ == Approx(6.1f));
+  REQUIRE(d.y_ == Approx(-8.3f));
+
+  d = b - c;
+  REQUIRE(d.x_ == Approx(4.1f));
+  REQUIRE(d.y_ == Approx(-10.3f));
+
+  d = 5 * c;
+  REQUIRE(d.x_ == Approx(5.0f));
+  REQUIRE(d.y_ == Approx(5.0f));
+
+  d = c * 5;
+  REQUIRE(d.x_ == Approx(5.0f));
+  REQUIRE(d.y_ == Approx(5.0f));
+
+  d = c / 5;
+  REQUIRE(d.x_ == Approx(0.2f));
+  REQUIRE(d.y_ == Approx(0.2f));
 }
 
 int main(int argc, char *argv[])
--- source/vec2.cpp
+++ source/vec2.cpp
@@ -1,25 +1,55 @@
 #include "vec2.hpp"
 
 Vec2& Vec2::operator +=(Vec2 const& v) {
-	x_ += v.x_;
-	y_ += v.y_;
-	return *this;
+  x_ += v.x_;
+  y_ += v.y_;
+  return *this;
 }
 
 Vec2& Vec2::operator -=(Vec2 const& v) {
-	x_ -= v.x_;
-	y_ -= v.y_;
-	return *this;
+  x_ -= v.x_;
+  y_ -= v.y_;
+  return *this;
 }
 
 Vec2& Vec2::operator *=(float s){
-	x_ *= s;
-	y_ *= s;
-	return *this;
+  x_ *= s;
+  y_ *= s;
+  return *this;
 }
 
 Vec2& Vec2::operator /=(float s) {
-	x_ /= s;
-	y_ /= s;
-	return *this;
+  x_ /= s;
+  y_ /= s;
+  return *this;
+}
+
+Vec2 operator +(Vec2 const& u, Vec2 const& v) {
+  Vec2 w = u;
+  w += v;
+  return w;
+}
+
+Vec2 operator -(Vec2 const& u, Vec2 const& v) {
+  Vec2 w = u;
+  w -= v;
+  return w;
+}
+
+Vec2 operator *(Vec2 const& v, float s) {
+  Vec2 w = v;
+  w *= s;
+  return w;
+}
+
+Vec2 operator /(Vec2 const& v, float s) {
+  Vec2 w = v;
+  w /= s;
+  return w;
+}
+
+Vec2 operator *(float s, Vec2 const& v) {
+  Vec2 w = v;
+  w *= s;
+  return w;
 }
