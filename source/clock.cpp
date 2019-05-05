cd3bec3448010ba30b759a11525b47c1267a6a3c
ChristopherScholl <christopher.scholl@online.de>
2019-05-05 18:24:45 +0200

    Added clock.cpp

--- source/clock.cpp
+++ source/clock.cpp
@@ -9,11 +9,12 @@
 #include <GLFW/glfw3.h>
 #include <utility>
 #include <cmath>
+#include <iostream>
 
 
 int main(int argc, char* argv[])
 {
-  Window win{std::make_pair(800,800)};
+  Window win{std::make_pair(500,500)};
 
   while (!win.should_close()) {
     if (win.get_key(GLFW_KEY_ESCAPE) == GLFW_PRESS) {
@@ -24,19 +25,6 @@ int main(int argc, char* argv[])
 
     auto t = win.get_time();
 
-    float x1 = 400.f + 380.f * std::sin(t);
-    float y1 = 400.f + 380.f * std::cos(t);
-
-    float x2 = 400.f + 380.f * std::sin(2.0f*t);
-    float y2 = 400.f + 380.f * std::cos(2.0f*t);
-
-    float x3 = 400.f + 380.f * std::sin(t-10.f);
-    float y3 = 400.f + 380.f * std::cos(t-10.f);
-
-    win.draw_point(x1, y1, 1.0f, 0.0f, 0.0f);
-    win.draw_point(x2, y2, 0.0f, 1.0f, 0.0f);
-    win.draw_point(x3, y3, 0.0f, 0.0f, 1.0f);
-
     auto mouse_position = win.mouse_position();
     if (left_pressed) {
       win.draw_line(30.0f, 30.0f, // FROM pixel idx with coords (x=30, y=30)
@@ -45,36 +33,13 @@ int main(int argc, char* argv[])
                     1.0);        // line thickness = 1.0 * default thickness
     }
 
-    win.draw_line(0, mouse_position.second, 10, mouse_position.second, 0.0, 0.0, 0.0);
-    win.draw_line(win.window_size().second - 10, mouse_position.second, win.window_size().second, mouse_position.second, 0.0, 0.0, 0.0);
+    Vec2 mid{ 250.0,225.0 };
+    Color white{ 1.0,1.0,1.0 };
+    Color red{ 1.0,0.0,0.0 };
+    Color black{ 0.0,0.0,0.0 };
 
-    win.draw_line(mouse_position.first, 0, mouse_position.first, 10, 0.0, 0.0, 0.0);
-    win.draw_line(mouse_position.first, win.window_size().second - 10, mouse_position.first, win.window_size().second, 0.0, 0.0, 0.0);
-
-    std::string display_text = "mouse position: (" + std::to_string(mouse_position.first) + ", " + std::to_string(mouse_position.second) + ")";
-    
-    int text_offset_x = 10;
-    int text_offset_y = 5;
-    unsigned int font_size = 35;
-    
-    win.draw_text(text_offset_x, text_offset_y, font_size, display_text);
-
-    Vec2 max{ 100.0,600.0 };
-    Vec2 min{ 300.0,200.0 };
-    Vec2 mid{ 600.0,400.0 };
-    Color col1{ 1.0,1.0,0.0 };
-    Color col2{ 1.0,0.0,1.0 };
-
-    Rectangle2 rec(min, max, col1);
     Vec2 mouse{ mouse_position.first, mouse_position.second };
-    if (rec.is_inside(mouse)) {
-      rec.draw(win, 2.0);
-    }
-    else {
-      rec.draw(win, 1.0);
-    }
-
-    Circle2 cir(mid, 100.0, col2);
+    Circle2 cir(mid, 200.0, white);
     if (cir.is_inside(mouse)) {
       cir.draw(win, 2.0);
     }
@@ -82,6 +47,26 @@ int main(int argc, char* argv[])
       cir.draw(win, 1.0);
     }
 
+    //std::cout << t << std::endl;
+    int h = (int)t / (60 * 60);
+    int m = ((int)t - h) / 60;
+    int s = (int)t - h - m;
+    h = h % 12;
+    m = m % 60;
+    s = s % 60;
+
+    std::cout << "Time: " << t << " Hours: " << h << " Minutes: " << m << " Seconds: " << s << std::endl;
+    float hr_x = (150.0) * -sin(6.28318530718 / 12 * h) + mid.x_;
+    float hr_y = (150.0) * cos(6.28318530718 / 12 * h) + mid.y_;
+    win.draw_line(250.0f, 250.0f, hr_x, hr_y + 25.0f, 0.0, 0.0, 0.0, 5.0);
+
+    float min_x = (200.0) * -sin(6.28318530718 / 60 * m) + mid.x_;
+    float min_y = (200.0) * cos(6.28318530718 / 60 * m) + mid.y_;
+    win.draw_line(250.0f, 250.0f, min_x, min_y + 25.0f, 0.0, 0.0, 0.0, 3.0);
+
+    float sec_x = (200.0) * -sin(6.28318530718 / 60 * s) + mid.x_;
+    float sec_y = (200.0) * cos(6.28318530718 / 60 * s) + mid.y_;
+    win.draw_line(250.0f, 250.0f, sec_x, sec_y + 25.0f, 1.0, 0.0, 0.0, 1.0);
 
     win.update();
   }
